5dccfd33b24728685514f405cfdeb5bb4ddc9c57
pfz <pfz@example.com>
2026-01-23 17:12:02 +0000

    Added back missing typing imports

--- textseal/wmtraining/train.py
+++ textseal/wmtraining/train.py
@@ -26,7 +26,7 @@ from contextlib import ExitStack
 from dataclasses import asdict, dataclass, field
 from pathlib import Path
 from timeit import default_timer as timer
-from typing import Any, Optional
+from typing import Any, Optional, Dict
 
 import numpy as np
 from omegaconf import OmegaConf
